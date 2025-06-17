1e0a66fbe547a7fd43fa42ccd2fac8b54f53e059
mattorb <matt@piikeastreet.com>
2025-06-17 05:37:58 -0500

    Initial commit for checkpoints

---
+++ TestCharts/TestChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestChartsApp.swift
+//  TestCharts
+//
+//  Created by Matthew Smith on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct TestChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
